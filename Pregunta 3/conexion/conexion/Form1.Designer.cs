f022344336ff313d2d432a41ca7959fa5091f517
RAQUEL ROCIO QUISPE ESCOBAR <130399081+raquel1ro@users.noreply.github.com>
2023-06-16 09:01:58 -0400

    Add files via upload

---
+++ Pregunta 3/conexion/conexion/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace conexion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnColorear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Silver;
+            this.btnColor.FlatAppearance.BorderSize = 0;
+            this.btnColor.ForeColor = System.Drawing.Color.Black;
+            this.btnColor.Location = new System.Drawing.Point(402, 311);
+            this.btnColor.Margin = new System.Windows.Forms.Padding(2);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(112, 38);
+            this.btnColor.TabIndex = 54;
+            this.btnColor.Text = "Establecer Color";
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnColorear
+            // 
+            this.btnColorear.BackColor = System.Drawing.Color.Silver;
+            this.btnColorear.FlatAppearance.BorderSize = 0;
+            this.btnColorear.ForeColor = System.Drawing.Color.Black;
+            this.btnColorear.Location = new System.Drawing.Point(402, 381);
+            this.btnColorear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnColorear.Name = "btnColorear";
+            this.btnColorear.Size = new System.Drawing.Size(112, 24);
+            this.btnColorear.TabIndex = 52;
+            this.btnColorear.Text = "Colorear";
+            this.btnColorear.UseVisualStyleBackColor = false;
+            this.btnColorear.Click += new System.EventHandler(this.btnColorear_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(538, 72);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(398, 142);
+            this.dataGridView1.TabIndex = 50;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(401, 145);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "Nombre de Textura";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(417, 169);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(113, 20);
+            this.txtNombre.TabIndex = 48;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.Silver;
+            this.btnGuardar.FlatAppearance.BorderSize = 0;
+            this.btnGuardar.ForeColor = System.Drawing.Color.Black;
+            this.btnGuardar.Location = new System.Drawing.Point(402, 353);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(112, 24);
+            this.btnGuardar.TabIndex = 47;
+            this.btnGuardar.Text = "Guardar BD";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(437, 114);
+            this.txtB.Margin = new System.Windows.Forms.Padding(2);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(76, 20);
+            this.txtB.TabIndex = 45;
+            this.txtB.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(398, 114);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "B:";
+            // 
+            // txtG
+            // 
+            this.txtG.Location = new System.Drawing.Point(437, 86);
+            this.txtG.Margin = new System.Windows.Forms.Padding(2);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(76, 20);
+            this.txtG.TabIndex = 43;
+            this.txtG.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(398, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "G:";
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(437, 60);
+            this.txtR.Margin = new System.Windows.Forms.Padding(2);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(76, 20);
+            this.txtR.TabIndex = 41;
+            this.txtR.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(399, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "R:";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.Color.Silver;
+            this.btnAbrir.FlatAppearance.BorderSize = 0;
+            this.btnAbrir.ForeColor = System.Drawing.Color.Black;
+            this.btnAbrir.Location = new System.Drawing.Point(404, 20);
+            this.btnAbrir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(112, 26);
+            this.btnAbrir.TabIndex = 39;
+            this.btnAbrir.Text = "Abrir Imagen";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 20);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(383, 566);
+            this.pictureBox1.TabIndex = 38;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(399, 201);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 13);
+            this.label5.TabIndex = 55;
+            this.label5.Text = "Valores para Nuevo Color:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(438, 279);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 61;
+            this.textBox1.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(399, 279);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 13);
+            this.label6.TabIndex = 60;
+            this.label6.Text = "B:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(438, 251);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 59;
+            this.textBox2.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Control;
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(399, 251);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 13);
+            this.label7.TabIndex = 58;
+            this.label7.Text = "G:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(438, 225);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 57;
+            this.textBox3.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(400, 225);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 56;
+            this.label8.Text = "R:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(947, 597);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnColorear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtG);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnColorear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+
+
+    }
+}
+
---
+++ Pregunta 3/conexion/conexion/Form1.cs
@@ -0,0 +1,196 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conexion
+{
+    public partial class Form1 : Form
+    {
+
+        private MySqlConnection cnx = null;
+        MySqlCommand cmd = null;
+        DataTable dt = null;
+        Conexion C = null;        
+        int RR, GG, BB;//BD
+        int meR, meG, meB;//media
+        int r2, g2, b2;//ncolor
+       
+        public Form1()
+        {
+            InitializeComponent();           
+            C = new Conexion();
+            cnx = C.Cnx();
+            Listar();
+        }
+
+        
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Todos|*.*|Archivos JPEG|*.jpg|Archivos GIF|*.gif";
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "")
+            {
+                Guardar(txtNombre.Text, int.Parse(txtR.Text), int.Parse(txtG.Text), int.Parse(txtB.Text), r2, g2, b2);
+                Listar();
+            }
+            else
+            {
+                MessageBox.Show("Escribe Nombre");
+            }
+        }
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            r2 = Int32.Parse(textBox3.Text);
+            g2 = Int32.Parse(textBox2.Text);
+            b2 = Int32.Parse(textBox1.Text);
+            MessageBox.Show("Color Establecido");
+        }
+        
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = new Color();
+            int x, y, mR = 0, mG = 0, mB = 0;
+            x = e.X; 
+            y = e.Y;
+            for (int i = x; i < x + 10; i++)
+                for (int j = y; j < y + 10; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    mR = mR + c.R;
+                    mG = mG + c.G;
+                    mB = mB + c.B;
+                }
+            mR = mR / 100;
+            mG = mG / 100;
+            mB = mB / 100;            
+            txtR.Text = mR.ToString();
+            txtG.Text = mG.ToString();
+            txtB.Text = mB.ToString();            
+        }
+
+
+        private void btnColorear_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width - 10; i += 10)
+                for (int j = 0; j < bmp.Height - 10; j += 10)
+                {
+                    meR = 0;
+                    meG = 0;
+                    meB = 0;
+
+                    for (int i2 = i; i2 < i + 10; i2++)
+                        for (int j2 = j; j2 < j + 10; j2++)
+                        {
+                            c = bmp.GetPixel(i2, j2);
+                            meR = meR + c.R;
+                            meG = meG + c.G;
+                            meB = meB + c.B;
+                        }
+                    meR = meR / 100;
+                    meG = meG / 100;
+                    meB = meB / 100;
+                    bool bandera = false;
+                    for (int k = 0; k < dt.Rows.Count; k++)
+                    {
+                        RR = int.Parse(dt.Rows[k][1].ToString());
+                        GG = int.Parse(dt.Rows[k][2].ToString());
+                        BB = int.Parse(dt.Rows[k][3].ToString());
+                        int RBD = int.Parse(dt.Rows[k][4].ToString());
+                        int GBD = int.Parse(dt.Rows[k][5].ToString());
+                        int BBD= int.Parse(dt.Rows[k][6].ToString());
+                        if (((RR - 10) < meR) && (meR < (RR + 10)) &&
+                        ((GG - 10) < meG) && (meG < (GG + 10)) &&
+                        ((BB - 10) < meB) && (meB < (BB + 10)))
+                            for (int i2 = i; i2 < i + 10; i2++)
+                                for (int j2 = j; j2 < j + 10; j2++)
+                                {
+                                    bmp2.SetPixel(i2, j2, Color.FromArgb(RBD, GBD, BBD));
+                                    bandera = true;
+                                }
+
+                    }
+                    if (!bandera)
+                    {
+                        for (int i2 = i; i2 < i + 10; i2++)
+                            for (int j2 = j; j2 < j + 10; j2++)
+                            {
+                                c = bmp.GetPixel(i2, j2);
+                                bmp2.SetPixel(i2, j2, c);
+
+                            }
+                    }
+                }
+            pictureBox1.Image = bmp2;
+        }                
+       
+
+       
+        
+        public void Guardar(string nomb, int r1, int g1, int b1,  int r2, int g2, int b2)
+        {
+            try
+            {                
+
+                cmd = new MySqlCommand();
+                cmd.Connection = cnx;               
+                cmd.CommandText = "INSERT INTO datos(Nombre,R1,G1,B1,R2,G2,B2 )  VALUES(@nomb,@r1,@g1,@b1,@r2,@g2,@b2 )";
+                cmd.Parameters.Add(new MySqlParameter("@nomb", nomb));
+                cmd.Parameters.Add(new MySqlParameter("@r1", r1));
+                cmd.Parameters.Add(new MySqlParameter("@g1", g1));
+                cmd.Parameters.Add(new MySqlParameter("@b1", b1));                
+                cmd.Parameters.Add(new MySqlParameter("@r2", r2));
+                cmd.Parameters.Add(new MySqlParameter("@g2", g2));
+                cmd.Parameters.Add(new MySqlParameter("@b2", b2));
+                cnx.Open();
+                MySqlDataReader lector = cmd.ExecuteReader();
+                MessageBox.Show("Se guardo correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            { cnx.Close(); }
+        }
+        private void Listar()
+        {
+            dt = new DataTable();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = cnx;
+            cmd.CommandText = "SELECT Nombre, R1, G1,B1,R2,G2,B2 FROM datos ORDER BY Id DESC"; ;
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        
+        
+
+        
+        
+
+    
+       
+
+    }   
+}
